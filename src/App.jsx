462921af7fbd0e8f615039eff7bf3ae35c255693
Ayden Budhoo <abudhoo@cs.stonybrook.edu>
2026-02-27 13:27:56 -0500

    Cleaned up splash page and header, made the caption for map on top of actual map

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
 import { SplashPage } from './components/SplashPage'
+import '../styles/main.css'
 
+/**
+ * Based on the current view (state variable), switch to it. Basically large switch statement
+ */
+function view()
+{
+  let view = 'Splash'
+  switch (view) 
+  {
+    case 'Splash':
+      {
+
+        break;
+      }
+      
+  
+    default:
+      break;
+  }
+}
 export default function App() {
   return (
     <div className="app-root">
--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {CountryHeaderBar} from './CountryHeaderBar.jsx'
-
+import '../../styles/splash-page.css'
 // ─────────────────────────────────────────────
 // SplashPage
 // ─────────────────────────────────────────────
@@ -13,6 +13,7 @@ export const SplashPage = () => (
     <CountryHeaderBar siteName="VRA Repeal Analysis" tabs={['Cross State Analysis', 'Another One']} />
     <div className="splashPage_body">
       {/* <h1 className="splashPage__title">Voting Rights Act Repeal Analysis</h1> */}
+      <div className='splashPage_mapHeader'>Country Map</div>
       <PlaceHolderMap />
     </div>
   </div>
