730f3cd248b0ea5c107f97bcc5267fc168bfbb23
Ayden Budhoo <abudhoo@cs.stonybrook.edu>
2026-03-03 10:51:51 -0500

    Got some of the interactive pieces working from custom analysis

--- src/components/StateCustomAnalysis.jsx
+++ src/components/StateCustomAnalysis.jsx
@@ -1,4 +1,6 @@
-
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import React from "react";
 /**
  * Export representation of data in the form of 
  * <div label>
@@ -79,8 +81,21 @@ function updateData(currentData,minoritySelection, secondData, thirdData)
                 }
                 
             }
+        case "GUI-5":
+        case "GUI-6":
+        case "GUI-7":
+        case "GUI-8":
+        case null:
+            {
+                const minorityHM =  <div className="minorityAnalysis_data">Assigned to {currentData}</div> // The actual heatmap
+                // Formatted in label data format
+                return displayData(<div className="minorityAnalysis_dataLabel">Some display of {currentData}</div>, minorityHM,"minorityAnalysis_dataContainer");
+
+            }
+                
     
         default:
+            console.error(`StateCustomAnalysis: updateData: currentData doesn't match a GUI usecase ${currentData}`)
             break;
     }
 }
@@ -128,7 +143,6 @@ function updateBody(minority, minorityList, minorityData, currentData1, secondDa
                     {firstData}
                     {secondData}
                     {thirdData}
-
     </div>);
     // (<div className="minorityAnalysis_dataBodyContainer">
     //                     {minorityHMWithLabel}
@@ -141,17 +155,24 @@ function updateBody(minority, minorityList, minorityData, currentData1, secondDa
 }
 function excludeDataFromDropdown(original,comparison1,comparison2)
 {
-    return ((comparison1 == <div>GUI 12</div>)|| (original != comparison1)) &&
-        ((original != comparison2 )|| (comparison2 == <div>GUI 12</div>))
+    return ((comparison1 == "GUI-12")|| (original != comparison1)) &&
+        ((original != comparison2 )|| (comparison2 == "GUI-12"))
 }
-function isMinorityDropdownDisabled(selection1,selection2,selection3)
+/**
+ * 
+ * @param {*} selection1 Value of GUI case
+ * @param {*} selection2 
+ * @param {*} selection3 
+ * @param {*} needsMinority List of GUI cases that need minorities
+ * @returns true if none needs it, false if they do
+ */
+function isMinorityDropdownDisabled(selection1,selection2,selection3,needsMinorityList)
 {
-    // Some actual matching logic to relevant race
-    if(selection1 == <div>GUI X</div>)
+    if(needsMinorityList.includes(selection1) || needsMinorityList.includes(selection2) || needsMinorityList.includes(selection3))
     {
-        return true;
+        return false
     }
-    return false
+    return true
 }
 
 /**
@@ -229,6 +250,9 @@ export default function StateCustomAnalysis(props)
     const { stateName } = useParams();
     // Find the relevant data for a minority given the state
     const minorityData = props.minorityData;
+    const extraDropDownDatas = [{name: 'GUI-12', numberExtra: 2},{name: 'GUI-16', numberExtra: 1}, {name: 'GUI-17', numberExtra: 1}];
+    const needsMinority = ['GUI-4','GUI-9','GUI-12'];
+
     let data = null;
     for(let d of minorityData)
     {
@@ -254,12 +278,19 @@ export default function StateCustomAnalysis(props)
     // Get minority and language lists
     let minorityList = data.minorityData.minorityList;
     let languageList = ["English", "Spanish", "French"];
+    console.log(`Minority List ${minorityList} data ${data}`)
+
+    const dataDescriptionList = ["GUI-4","GUI-5", "GUI-6","GUI-7", "GUI-8"]; // The list holding the description of all the charts, graphs, and tables in a ascending GUI usecase order
+    const dataOptions = dataDescriptionList.map((data,index) => <option key={`Data-Descriptions-${index}`} value={data}>{data}</option>)
+
+    const minorityOptions = minorityList.map((minority)=> <option key={`Minority-Options-${minority}`} value={minority}>{minority}</option>)
+
     // Options for dropdowns
     const [currentMinority, changeMinority] = useState(minorityList[0]); // Record the option chosen inside 
     // Data display dropdowns
-    const [currentData1, changeData1] = useState("null");
-    const [currentData2, changeData2] = useState(null);
-    const [currentData3, changeData3] = useState(null);
+    const [currentData1, changeData1] = useState(dataDescriptionList[0]);
+    const [currentData2, changeData2] = useState(dataDescriptionList[1]);
+    const [currentData3, changeData3] = useState(dataDescriptionList[2]);
     // Need to make sure these are "reset" whenever one of the higher ups are changed as to set a normal default value, if used for other gui use cases
         // Do I? I think its auto-set with the <select value={}> thing
     
@@ -274,35 +305,32 @@ export default function StateCustomAnalysis(props)
 
 
 
-    const dataDescriptionList = ["GUI-4","GUI-5", "GUI-6","GUI-7", "GUI-8"]; // The list holding the description of all the charts, graphs, and tables in a ascending GUI usecase order
-    const dataOptions = dataDescriptionList.map((data,index) => <option key={`data ${index}`} value={`data ${index}`}>{data}</option>)
-
-    const minorityOptions = minorityList.map((minority)=> <option key={minority} value={minority}>{minority}</option>)
-
     // Display the minority content
-    let displayBody = updateBody(currentMinority,minorityList,props.minorityData);
+    let displayBody = updateBody(currentMinority,minorityList,props.minorityData,
+                                currentData1,secondData1,thirdData1,currentData2,secondData2,thirdData2,
+                                currentData3,secondData3,thirdData3);
     return(
     <div className="customAnalysis_bodyContainer">
     <div className="customAnalysis_minorityCheckboxContainer">
         <label htmlFor="minoritySelector">Choose the Minority to analyze: </label>
-        <select name="minoritySelector" id="minoritySelector" disabled={isMinorityDropdownDisabled(currentData1,currentData2,currentData3)} value={currentMinority} onChange={(e) => changeMinority(e.target.value)}>
+        <select name="minoritySelector" id="minoritySelector" disabled={isMinorityDropdownDisabled(currentData1,currentData2,currentData3, needsMinority)} value={currentMinority} onChange={(e) => changeMinority(e.target.value)}>
         {minorityOptions}
         </select>
     </div>
     <div className="customAnalysis_dataCheckboxContainer">
         {/* <label htmlFor="dataSelector">Choose the Minority to analyze: </label> */}
         <select name="dataSelector1" id="dataSelector1" value={currentData1} onChange={(e) => {changeData1(e.target.value); changeSecondData1(null); changeThirdData1(null);}}> {/* Update value and reset extra dropdowns */}
-        {dataOptions.filter((data) => excludeDataFromDropdown(data,currentData2,currentData3))} {/* Block out the values the other ones are using, unless its GUI 12 */}
+        {dataOptions.filter((data) => excludeDataFromDropdown(data.props.value,currentData2,currentData3))} {/* Block out the values the other ones are using, unless its GUI 12 */}
         </select>
         {returnExtraDropdownsWithLabels(1,currentData1,secondData1,changeSecondData1,thirdData1,changeThirdData1, minorityList, languageList)}
         
         <select name="dataSelector2" id="dataSelector2" value={currentData2} onChange={(e) => {changeData2(e.target.value); changeSecondData2(null); changeThirdData2(null);}}>
-        {dataOptions.filter((data) => excludeDataFromDropdown(data,currentData1,currentData3))}
+        {dataOptions.filter((data) => excludeDataFromDropdown(data.props.value,currentData1,currentData3))}
         </select>
         {returnExtraDropdownsWithLabels(2,currentData2,secondData2,changeSecondData2,thirdData2,changeThirdData2, minorityList, languageList)}
 
         <select name="dataSelector3" id="dataSelector3" value={currentData3} onChange={(e) => {changeData3(e.target.value); changeSecondData3(null); changeThirdData3(null);}}>
-        {dataOptions.filter((data) => excludeDataFromDropdown(data,currentData1,currentData2))}
+        {dataOptions.filter((data) => excludeDataFromDropdown(data.props.value,currentData1,currentData2))}
         </select>
         {returnExtraDropdownsWithLabels(3,currentData3,secondData3,changeSecondData3,thirdData3,changeThirdData3, minorityList, languageList)}
     </div>
