84d589e9c5267a0de178483290333dcf851677e4
Sahil Parikh <parikhsahil719@gmail.com>
2026-03-09 18:37:00 -0400

    refactor(api): switch base path from /api/v1 to /api

--- server/src/main/java/edu/stonybrook/cse416/braves/server/api/StateController.java
+++ server/src/main/java/edu/stonybrook/cse416/braves/server/api/StateController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api")
 @Validated
 public class StateController {
     private final BackendDataService dataService;
